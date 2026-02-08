ed76ee5526dc0ab86036d95bed01922d7ea0b0d1
vipaka2 <pakarinen.ville30@gmail.com>
2026-02-08 18:56:26 +0200

    Updated app main page file

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main style={{ padding: "50px", fontFamily: "sans-serif" }}>
       <h1>Work in Progress. New site coming soon.</h1>
+      <h2>Contact: </h2>
       <a 
         href="mailto:ville@villepakarinen.com" 
         style={{ marginTop: "2rem", color: "#fff", textDecoration: "underline" }}
